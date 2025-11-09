2c970f627928f2f549bb9492038b712d89dcb225
Sayam <sayammaha10@gmail.com>
2025-11-09 11:12:53 +0545

    Fix: render 'home' not 'Home'

--- src/index.js
+++ src/index.js
@@ -67,7 +67,7 @@ app.post("/login", async (req, res) => {
       checkUser.password
     );
     if (passwordMatch) {
-      return res.render("Home");
+      return res.render("home");
     } else {
       return res.send("Wrong password.");
     }
@@ -76,7 +76,7 @@ app.post("/login", async (req, res) => {
   }
 });
 
-const port = 5000;
-app.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
 });
